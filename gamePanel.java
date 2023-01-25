6cb9946d2728e0b0f0da51ea5fdaef2d6af6e5ec
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-01-25 12:09:47 -0700

    added dimensions for game panel

--- gamePanel.java
+++ gamePanel.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 
 
-public  class gamePanel extends JPanel implements ActionListener{
+public  class GamePanel extends JPanel implements ActionListener{
 
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
@@ -36,18 +36,36 @@ public  class gamePanel extends JPanel implements ActionListener{
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
-        this.setFocusable
+        this.setFocusable(true);
+        this.addKeyListener(new myKeyAdaptor());
+        startGame();
             
     }
     //lets create a method called start game 
 
     public void startGame() {
+        //add an apple on the screen
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+
 
     }
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
 
     }
     public void draw(Graphics g) {
+        //create grid
+        for (int i=0; i< SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            g.drawLine(i *UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i *UNIT_SIZE);
+        }
+
+    }
+    public void newApple(){
 
     }
 
